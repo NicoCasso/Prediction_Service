d20310a240a05b4b0986aee1bebc8dd2f25a6f4f
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-02-18 12:01:24 +0100

    modification of datetime

--- dependencies.py
+++ dependencies.py
@@ -13,9 +13,9 @@ ALGORITHM = "HS256"
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.timezone.utc() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.timezone.utc() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
