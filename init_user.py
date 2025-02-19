1feb3336104499233b95102bc4a2ddace496b90b
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-02-19 15:19:48 +0100

    comment utiliser le token ???

--- init_user.py
+++ init_user.py
@@ -3,26 +3,49 @@ from config import DATABASE_URL
 from models import User
 
 
-user_email = "nicolas.cassonnet@wanadoo.fr"
-user_password = "nicolas.cassonnet@wanadoo.fr"
-user_name = "Nicolas"
-user_role = "admin"
+
+users = []
+users.append({ 
+    "username" : "Nicolas",
+    "email" : "nicolas.cassonnet@wanadoo.fr",
+    "is_active" : True, 
+    "password" : "nicolas.cassonnet@wanadoo.fr",
+    "role"  : "admin",
+})
+users.append({ 
+    "username" : "User1",
+    "email" : "user1.fakemail@fakeprovider.com",
+    "is_active" : False, 
+    "password" : "initialpass1",
+    "role"  : "user"
+})
+users.append({
+    "username" : "User2",
+    "email" : "user2.fakemail@fakeprovider.com",
+    "is_active" : False, 
+    "password" : "initialpass2",
+    "role"  : "user"
+})
+
 def init() :
 
     engine = create_engine(DATABASE_URL)
 
     # Ouvrir une session
     with Session(engine) as session:
-        # Sélection de tous les utilisateurs
-        statement = select(User).where(User.email==user_email)
-        result = session.exec(statement).one_or_none()
-        if not result :
-            new_user= User(email=user_email)
-            new_user.set_password(user_password)
-            new_user.username = user_name
-            new_user.role = user_role
-            session.add(new_user)
-            session.commit()
+        # Sélection de chaque utilisateur à ajouter
+
+        for user in users :
+            statement = select(User).where(User.username==user["username"])
+            result = session.exec(statement).one_or_none()
+            if not result :
+                new_user= User(email=user["email"])
+                new_user.username = username=user["username"]
+                new_user.set_password(user["password"])
+                new_user.role = user["role"]
+                new_user.is_active = user["is_active"]
+                session.add(new_user)
+                session.commit()
 
     print("done")
 
--- test_app.py
+++ test_app.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from fastapi.security import OAuth2PasswordBearer
+
 from endpoints import auth, loans, admin
 
-import pytest
-from unittest.mock import patch, MagicMock
 from dependencies import get_current_admin, get_current_user, verify_token
 from db_session_provider import get_db_session
+import init_user 
 
+# Créer une instance de l'application FastAPI
 app = FastAPI()
 
 # Inclure les routes définies dans les fichiers séparés
@@ -14,111 +16,83 @@ app.include_router(auth.router)
 app.include_router(loans.router)
 app.include_router(admin.router)
 
-# Client de test FastAPI
-@pytest.fixture
-def client():
-    return TestClient(app)
 
-# Fixture pour un utilisateur avec un rôle 'admin'
-@pytest.fixture
-def mock_admin_user():
-    return {
-        "email": "nicolas.cassonnet@wanadoo.fr",
-        "password": "nicolas.cassonnet@wanadoo.fr"  # Le mot de passe en clair pour se connecter
-    }
+# # Définir un schéma d'authentification
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# Fixture pour mocker la session de base de données
-@pytest.fixture
-def mock_db_session():
-    mock_session = MagicMock()
-    mock_query = MagicMock()
-    mock_session.query.return_value = mock_query
-    yield mock_session
+client = TestClient(app)
 
-# Fixture pour simuler un utilisateur actuel
-@pytest.fixture
-def mock_current_user():
-    mock_user = MagicMock()
-    mock_user.id = 1
-    mock_user.email = "nicolas.cassonnet@wanadoo.fr"
-    return mock_user
-
-# Fixture pour mocker `verify_token`
-@pytest.fixture
-def mock_verify_token():
-    return "mocked_token"
-
-# Reset des overrides après chaque test
-@pytest.fixture(autouse=True)
-def clear_dependency_overrides():
-    app.dependency_overrides = {}
-    yield
-    app.dependency_overrides = {}
+users = init_user.users
+admin_user = users[0]
+client_user_1 = users[1]
+client_user_2 = users[2]
 
 #______________________________________________________________________________
 #
 # region 1 : auth login
 #______________________________________________________________________________
-def test_login_OK(client: TestClient, mock_admin_user, mock_db_session):
+def test_login_OK():
     """Test pour obtenir un token d'accès valide"""
 
-    # Simuler la récupération de l'utilisateur dans la base de données
-    mock_db_session.query.return_value.filter.return_value.first.return_value = mock_admin_user
-    mock_admin_user["verify_password"] = lambda password: password == mock_admin_user["password"]
-
-    # Envoie une requête POST pour se connecter et obtenir un token d'accès
-    response = client.post("/auth/login",
-        json= {
-            "email": mock_admin_user["email"],
-            "password": mock_admin_user["password"]
-        })
+    user = admin_user
+    response = get_login_response(admin_user)
 
     assert response.status_code == 200
     assert "access_token" in response.json()
-    access_token = response.json()["access_token"]
+
+def get_login_response(user) :
+    # Envoie une requête POST pour se connecter et obtenir un token d'accès
+    response = client.post("/auth/login",
+        json= {
+            "email": admin_user["email"],
+            "password": admin_user["password"]
+        })
+    return response
+    
+def get_token(loginresponse):
+    access_token = loginresponse.json()["access_token"]
     return access_token
+
+def get_headers(jwt_token) :
+    str_token = str(jwt_token)
+    headers = {
+        'Authorization': f'Bearer {str_token}',  # Ajout du token dans l'en-tête Authorization
+    }
+    return headers
 
 #______________________________________________________________________________
 #
 # region 2 : auth activation
 #______________________________________________________________________________
-def test_activation_OK(client: TestClient, mock_db_session, mock_current_user):
+def test_activation_OK():
+    
+    user_login_response = get_login_response(client_user_1)
+    jwt_token = get_token(user_login_response)
+    headers = get_headers(jwt_token)
+
     # Données d'activation
-    activation_data = {"email": mock_current_user.email, "password": "new_password"}
-
-    # Mock de `get_current_user` pour renvoyer l'utilisateur en cours
-    app.dependency_overrides[get_current_user] = lambda: mock_current_user  # L'utilisateur courant est un admin
-
-    # Mock de la fonction `verify_token`
-    app.dependency_overrides[verify_token] = lambda: "valid_token"
-
-    # Mock de la session de base de données
-    mock_db_session.query.return_value.filter.return_value.first.return_value = mock_current_user  # L'utilisateur existe dans la base
-
-    # Simuler l'appel à la route d'activation
-    response = client.post("/auth/activation", json=activation_data)
+    activation_data = {"email": str(client_user_1["email"]), "password": "otherpass1"}
+    
+    # Appel à la route d'activation
+    response = client.post("/auth/activation", json=activation_data, headers= headers)
 
     # Vérifications
     assert response.status_code == 200
     assert response.json()["msg"] == "Account activated and password updated successfully."
-    assert mock_db_session.commit.called
-    assert mock_db_session.refresh.called
 
-    # Vérification que l'utilisateur est activé et que le mot de passe a été changé
-    updated_user = mock_db_session.query.return_value.filter.return_value.first.return_value
-    assert updated_user.is_active is True
-    assert updated_user.password == "new_password"
 
 #______________________________________________________________________________
 #
 # region 3 : auth logout
 #______________________________________________________________________________
-def test_logout_OK(client: TestClient):
-    # Mock de la fonction `verify_token` pour valider le token
-    app.dependency_overrides[verify_token] = lambda: "valid_token"
+def test_logout_OK():
 
-    # Simuler l'appel à la route de déconnexion
-    response = client.post("/auth/logout")
+    admin_login_response = get_login_response(client_user_1)
+    jwt_token = get_token(admin_login_response)
+    headers = get_headers(jwt_token)
+
+    # Appel à la route de déconnexion
+    response = client.post("/auth/logout", headers=headers)
 
     # Vérifications
     assert response.status_code == 200
@@ -128,12 +102,14 @@ def test_logout_OK(client: TestClient):
 #
 # region 4 : loans predict
 #______________________________________________________________________________
-def test_loans_predict_OK(client: TestClient, mock_verify_token):
-    # Mock de la fonction `get_current_user` pour retourner un utilisateur simulé
-    client.app.dependency_overrides[verify_token] = lambda: mock_verify_token
+def test_loans_predict_OK():
 
-    # Simuler l'appel à la route de prédiction d'éligibilité
-    response = client.get("/loans/predict", headers={"Authorization": "Bearer mock_token"})
+    user_login_response = get_login_response(client_user_1)
+    jwt_token = get_token(user_login_response)
+    headers = get_headers(jwt_token)
+
+    # Appel à la route de prédiction d'éligibilité
+    response = client.get("/loans/predict", headers=headers)
 
     # Vérifications
     assert response.status_code == 200
@@ -143,38 +119,35 @@ def test_loans_predict_OK(client: TestClient, mock_verify_token):
 #
 # region 5 : loans request
 #______________________________________________________________________________
-def test_loans_request_OK(client: TestClient, mock_verify_token, mock_db_session, mock_current_user):
-    # Mock de la fonction `get_current_user` pour retourner un utilisateur simulé
-    client.app.dependency_overrides[verify_token] = lambda: mock_verify_token
-    client.app.dependency_overrides[get_current_user] = lambda: mock_current_user
-    client.app.dependency_overrides[get_db_session] = lambda: mock_db_session
+def test_loans_request_OK():
+
+    user_login_response = get_login_response(client_user_1)
+    jwt_token = get_token(user_login_response)
+    headers = get_headers(jwt_token)
 
     # Données de la demande de prêt
     loan_request_data = {"amount": 10000, "term": 12}
 
-    # Simuler l'appel à la route de soumission de la demande de prêt
+    # Appel à la route de soumission de la demande de prêt
     response = client.post("/loans/request", json=loan_request_data, headers={"Authorization": "Bearer mock_token"})
 
     # Vérifications
     assert response.status_code == 200
     assert response.json() == {"msg": "Loan request submitted successfully."}
 
-    # Vérifie que la demande de prêt a bien été ajoutée à la base de données
-    mock_db_session.add.assert_called_once()  # Vérifie que `add` a été appelé une fois
-    mock_db_session.commit.assert_called_once()  # Vérifie que `commit` a été appelé une fois
 
 #______________________________________________________________________________
 #
 # region 6 : loans history
 #______________________________________________________________________________
-def test_loans_history_OK(client: TestClient, mock_verify_token, mock_db_session, mock_current_user):
-    # Mock de la fonction `get_current_user` pour retourner un utilisateur simulé
-    client.app.dependency_overrides[verify_token] = lambda: mock_verify_token
-    client.app.dependency_overrides[get_current_user] = lambda: mock_current_user
-    client.app.dependency_overrides[get_db_session] = lambda: mock_db_session
+def test_loans_history_OK():
 
-    # Simuler l'appel à la route d'historique des demandes de prêt
-    response = client.get("/loans/history", headers={"Authorization": "Bearer mock_token"})
+    user_login_response = get_login_response(client_user_1)
+    jwt_token = get_token(user_login_response)
+    headers = get_headers(jwt_token)
+
+    # Appel à la route d'historique des demandes de prêt
+    response = client.get("/loans/history", headers=headers)
 
     # Vérifications
     assert response.status_code == 200
@@ -185,49 +158,42 @@ def test_loans_history_OK(client: TestClient, mock_verify_token, mock_db_session
 
 #______________________________________________________________________________
 #
-# region 7 : admin users après une connexion réussie
+# region 7 : admin users
 #______________________________________________________________________________
-def test_get_users_OK(client: TestClient, mock_admin_user, mock_db_session):
-    """Test pour récupérer la liste des utilisateurs avec un token d'accès"""
+def test_get_users_OK():
 
-    # Utilisation du token obtenu de la méthode de connexion pour accéder à la ressource protégée
-    access_token = test_login_OK(client, mock_admin_user, mock_db_session)
-
-    # Simuler la récupération des utilisateurs dans la base de données
-    mock_db_session.query.return_value.all.return_value = [
-        {"email": "user1@example.com"},
-        {"email": "user2@example.com"}
-    ]
+    admin_login_response = get_login_response(admin_user)
+    jwt_token = get_token(admin_login_response)
+    headers = get_headers(jwt_token)
 
     # Envoie une requête GET pour récupérer la liste des utilisateurs
-    response = client.get(
-        "/admin/users",
-        headers={"Authorization": f"Bearer {access_token}"}
-    )
+    response = client.get("/admin/users", headers=headers )
 
     # Vérifie la réponse et les résultats attendus
     assert response.status_code == 200
     assert "users" in response.json()
-    assert len(response.json()["users"]) == 2
-    assert response.json()["users"][0]["email"] == "user1@example.com"
+    assert len(response.json()["users"]) == len(users)
+    assert response.json()["users"][1]["email"] == client_user_1["email"]
 
 #______________________________________________________________________________
 #
-# region 8 : Test de la création d'un utilisateur
+# region 8 : create_user
 #______________________________________________________________________________
-def test_create_user_OK(client: TestClient, mock_db_session, mock_admin_user):
+def test_create_user_OK():
+    
+    admin_login_response = get_login_response(admin_user)
+    jwt_token = get_token(admin_login_response)
+    headers = get_headers(jwt_token)
+
     # Données utilisateur à envoyer
     user_data = {
-        "email": "new_user@example.com",
-        "password": "password123"
+        "username" : "User3",
+        "email" : "user3.fakemail@fakeprovider.com",
+        "is_active" : False, 
+        "password" : "initialpass3",
+        "role"  : "user"
     }
-
-    # Mock de la fonction `get_current_admin` pour retourner un utilisateur admin simulé
-    client.app.dependency_overrides[get_current_admin] = lambda: mock_admin_user
-
-    # Mock de la méthode `session.add` et `session.commit`
-    mock_db_session.add = MagicMock()
-    mock_db_session.commit = MagicMock()
+    users.append(user_data)
 
     # Simuler l'appel à la route de création d'utilisateur
     response = client.post("/admin/users", json=user_data)
@@ -235,10 +201,16 @@ def test_create_user_OK(client: TestClient, mock_db_session, mock_admin_user):
     # Vérifications
     assert response.status_code == 200
     assert response.json()["email"] == user_data["email"]
-    assert mock_db_session.add.called
-    assert mock_db_session.commit.called
 
-    # Vérifier que l'utilisateur a bien été créé dans la base de données
-    created_user = mock_db_session.add.call_args[0][0]
-    assert created_user.email == user_data["email"]
+if __name__ == "__main__" :
+    test_login_OK()
+    #test_logout_OK()
+    #test_activation_OK()
+
+    test_get_users_OK()
+    test_create_user_OK()
+
+    test_loans_predict_OK()
+    test_loans_history_OK()
+    test_loans_request_OK()
 
